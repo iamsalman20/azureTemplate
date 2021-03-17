bf31618e9a94140b3abf5bfc490f431387b3301d
Salman Noshad <NoshadMu@gi-de.com>
2021-03-17 16:31:55 +0100

    removed user creation

--- sftpvmconfig.sh
+++ sftpvmconfig.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DNS="https://127.0.0.1/backend"
-wget "https://raw.githubusercontent.com/iamsalman20/azureTemplate/master/users.json"
+#wget "https://raw.githubusercontent.com/iamsalman20/azureTemplate/master/users.json"
 sleep 3s
 CONFIG_FILE="users.json"
 while getopts ":P:K:S:" opt; do
@@ -28,41 +28,5 @@ echo "${storage_account}" >> /home/ubuntu/scriptoutput.txt
 TOKEN=$(curl -X POST "${DNS}/login" -H 'Content-Type: application/json' -d '{ "username": "admin", "password": "'${admin_password}'" }' -k)
 curl -X PUT "${DNS}/api/configuration" -H 'Content-Type: application/json'  -H "authorization: Bearer ${TOKEN}" -d '{ "usesAzureStorageKey": true, "azureStorageKey": "'${blob_account_key}'", "azureStorageName": "'${storage_account}'" }' -k
 
-
-
-ALL_USERS=$(curl -X GET "${DNS}/api/users" -H 'Content-Type: application/json' -H "authorization: Bearer ${TOKEN}" -k)
-
-jq -r -c '.users[]' "${CONFIG_FILE}" | while read RAW_USER; do
-    USER_PROPS=$(echo ${RAW_USER} | tr -d '\r')
-    USER=$(jq -n $USER_PROPS | jq -r .name)
-    BUCKET_NAME=$(jq -n $USER_PROPS | jq -r .bucket_name)
-    if [[ ! $(echo $ALL_USERS | jq -r --arg USER "$USER" '.users[]  | select(.username==$USER)') ]]; then
-        echo "creating new user: ${USER}" >> /home/ubuntu/scriptoutput.txt
-#        SSH_KEY=$(curl -X GET "${DNS}/api/keypair/generate" \
-#            -H 'Content-Type: application/json' \
-#            -H "authorization: Bearer ${TOKEN}" \
-#            -k)
-
- #       PUB_KEY=$(echo "${SSH_KEY}" | jq -r .publicKey)
- #       PRI_KEY=$(echo "${SSH_KEY}" | jq -r .privateKey)
-
-  #      echo "${PUB_KEY}" > "${USER}.key.pub"
-  #      echo "${PRI_KEY}" > "${USER}.key"
-
-  #      az keyvault secret set --name "vm${postfix}${USER}pub" --vault-name "${KEYVAULT_NAME}" --file "${USER}.key.pub"
-  #      az keyvault secret set --name "vm${postfix}${USER}pri" --vault-name "${KEYVAULT_NAME}" --file "${USER}.key"
-
-   #     rm "${USER}.key.pub"
-    #    rm "${USER}.key"
-            
-        # TODO put in function
-    #    USER_PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-    #    az keyvault secret set --name "vm${postfix}${USER}pwd" --vault-name ${KEYVAULT_NAME} --value ${USER_PASSWORD}
-
- #       curl -X POST "${DNS}/api/users" \
-#            -H 'Content-Type: application/json' \
-  #          -H "authorization: Bearer ${TOKEN}" \
-  #          -d '{"username":"'${USER}'","password":"'${USER_PASSWORD}'","enableUploadDir":true,"bucketName":"'${BUCKET_NAME}'","path":"'${USER}'","pubSsh":"'"${PUB_KEY}"'"}' \
-  #          -k
-    fi
-done
+#SSH_KEY=$(curl -X GET "${DNS}/api/keypair/generate"  -H "authorization: Bearer ${TOKEN}" -k)
+#echo "${SSH_KEY}" >> /home/ubuntu/scriptoutput.txt
