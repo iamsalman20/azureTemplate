10dce87b56cebd254dcea1bcdfa7ce691e104eae
Salman Noshad <NoshadMu@gi-de.com>
2021-03-19 17:24:52 +0100

    added users file download

--- sftpvmconfig.sh
+++ sftpvmconfig.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DNS="https://127.0.0.1/backend"
-#wget "https://raw.githubusercontent.com/iamsalman20/azureTemplate/master/users.json"
+wget "https://raw.githubusercontent.com/iamsalman20/azureTemplate/master/users.json"
 sleep 3s
 CONFIG_FILE="users.json"
 while getopts ":P:K:S:" opt; do
@@ -28,5 +28,3 @@ echo "${storage_account}" >> /home/ubuntu/scriptoutput.txt
 TOKEN=$(curl -X POST "${DNS}/login" -H 'Content-Type: application/json' -d '{ "username": "admin", "password": "'${admin_password}'" }' -k)
 curl -X PUT "${DNS}/api/configuration" -H 'Content-Type: application/json'  -H "authorization: Bearer ${TOKEN}" -d '{ "usesAzureStorageKey": true, "azureStorageKey": "'${blob_account_key}'", "azureStorageName": "'${storage_account}'" }' -k
 
-#SSH_KEY=$(curl -X GET "${DNS}/api/keypair/generate"  -H "authorization: Bearer ${TOKEN}" -k)
-#echo "${SSH_KEY}" >> /home/ubuntu/scriptoutput.txt
