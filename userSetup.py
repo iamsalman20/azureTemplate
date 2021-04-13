538e13c898bcc5406692672cf0480952b045d61b
Salman Noshad <NoshadMu@gi-de.com>
2021-04-13 16:16:13 +0200

    removed download from github dependency

--- userSetup.py
+++ userSetup.py
@@ -31,58 +31,69 @@ login_data = {"username": "admin", "password": options.passwd }
 config_data = {"azureStorageName": options.storageName, "azureStorageKey": options.storageKey, "usesAzureStorageKey": True }
 existingUserFileName = 'exisUsers.json'
 newUsersFile = 'users.json'
+logFile = 'userSetupLogs.txt'
+logsFolder = '/home/ubuntu/installationLogs'
 
-response = urllib2.urlopen('https://raw.githubusercontent.com/iamsalman20/azureTemplate/master/users.json')
-users = open( 'newUsersFile', 'w' )
-users.write( response.read() )
-users.close()
+if not os.path.exists(logsFolder):
+    os.makedirs(logsFolder)
+
+logs = open( os.path.join(logsFolder, logFile), 'w' )
 
 TOKEN = requests.post(URL_login, headers={"Content-Type":"application/json"}, json=login_data, verify=False)
+logs.write( 'Successfully Logged in with Token: ' + TOKEN.text)
+logs.write( '\nUpdating Storage Account Configs\n' )
 
 requests.put(URL_config, headers={"Content-Type":"application/json", "authorization": "Bearer " + TOKEN.text}, json=config_data, verify=False)
   
 existing_users = requests.get(URL_users, headers={"Content-Type":"application/json", "authorization": "Bearer " + TOKEN.text}, verify=False)
+
 use = open( existingUserFileName, 'w' )
 use.write( existing_users.text )
 use.close()
 exisUsersList = []
 
+logs.write( 'Currently configured users: \n')
 with open(existingUserFileName) as json_users:
     exiusers = json.load(json_users)
     for u in exiusers['users']:
     	userName= u['username']
     	exisUsersList.append(userName)
+    	logs.write(userName + '\n')
     	
 if os.path.exists(existingUserFileName):
   os.remove(existingUserFileName)
 
+logs.write( 'Logging into azure using service principal \n' )
 logincmd = ['az', 'login', '--service-principal','--username', options.clientId, '--password', options.clientSec, '--tenant', options.tenantId]
 subprocess.call(logincmd)
 
 
-with open('newUsersFile') as json_file:
+with open(newUsersFile) as json_file:
     users = json.load(json_file)
     for p in users['users']:
     	userName= p['name']
     	if userName in exisUsersList :
-    		print("User Already exists")
+    		logs.write( 'User ' + userName + ' already exists \n' )
     	else:
-    		print("Creating User...")
     		bucketName = p['bucket_name']
+    		logs.write( 'Generating SSH Keys for user: ' + userName + '\n' )
         	sshKeys = requests.get(URL_sshGen, headers={"Content-Type":"application/json", "authorization": "Bearer " + TOKEN.text}, verify=False)
 		jsonKeys = json.loads(sshKeys.text)
 		pubKey = jsonKeys['publicKey']
 		priKey = jsonKeys['privateKey']
 		secpubKey = userName + 'pub'
 		secpriKey = userName + 'pri'
+		logs.write( 'Saving Pub and pri keys into keyvault for user: ' + userName + '\n' )
 		keyvaultcmd = ['az', 'keyvault', 'secret', 'set', '--name', secpubKey, "--vault-name", 'uatsftp-keyvault', '--value', pubKey]
 		subprocess.call(keyvaultcmd)
 		keyvaultcmd = ['az', 'keyvault', 'secret', 'set', '--name', secpriKey, "--vault-name", 'uatsftp-keyvault', '--value', priKey]
 		subprocess.call(keyvaultcmd)
 		userPass = passwd_generator(32)
 		secPwdKey = userName + 'pwd'
+		logs.write( 'Saving SFTP Passwd into Keyvault for user: ' + userName + '\n' )
 		keyvaultcmd = ['az', 'keyvault', 'secret', 'set', '--name', secPwdKey, "--vault-name", 'uatsftp-keyvault', '--value', userPass]
 		subprocess.call(keyvaultcmd)
 		user_data = {"username": userName, "password": userPass, "enableUploadDir": True, "bucketName": bucketName, "path": userName, "pubSsh": pubKey }
+		logs.write( 'Creating User... ' + userName + '\n' )
 		requests.post(URL_users, headers={"Content-Type":"application/json", "authorization": "Bearer " + TOKEN.text}, json=user_data, verify=False)
-
+logs.close()
